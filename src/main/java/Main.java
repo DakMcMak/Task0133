7876f52d95d372dc15f58dd98789222bb4b0afb9
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-23 23:46:07 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getCountSecondsPassedInCurrentHour(12345));
+    }
+
+    public static int getCountSecondsPassedInCurrentHour(int seconds) {
+        int hour = seconds % 3600;
+        return hour;
+    }
+}
